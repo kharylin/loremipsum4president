2351c2244ca1aa831a9b55c0ad44967f182d192e
kharylin <38194352+kharylin@users.noreply.github.com>
2018-04-08 20:20:16 -0300

    Update content.js

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var politicsSUX = text.replace(/Corinthians|Timão|corinthians|palmeiras|Palmeiras/g, ' ');
+            var politicsSUX = text.replace(/Corinthians|Timão|corinthians|palmeiras|la casa de papel|tokio|tokyo|Palmeiras/g, ' ');
 
             if (politicsSUX!== text) {
                 element.replaceChild(document.createTextNode(politicsSUX), node);
